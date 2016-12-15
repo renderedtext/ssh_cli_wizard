bd0d38d06beb912878f0209b2eb9451f04cca7d3
milica-nerlovic <milica.nerlovic@gmail.com>
2016-12-15 13:17:07 +0100

    User can show all commands

--- lib/wizard.ex
+++ lib/wizard.ex
@@ -6,16 +6,19 @@ defmodule Wizard do
     job = Parser.parse
 
     [user_command | user_command_set] = args
+    [command_set | rest] = user_command_set
 
-    if user_command == "run" do
-      [command_set | rest] = user_command_set
-      if job |> Map.get("commands") |> Map.has_key?(command_set) do
-       Parser.commands(job, command_set) |> Runner.execute
-      else
-        IO.puts "These are not the droids you're looking for. There are no such commands."
-      end
-    else
-      IO.puts "You can only run commands. This is not a drill."
+    case user_command do
+      "run" ->
+          if job |> Map.get("commands") |> Map.has_key?(command_set) do
+           Parser.commands(job, command_set) |> Runner.execute
+          else
+            IO.puts "These are not the droids you're looking for. There are no such commands."
+          end
+      "show" ->
+          #here all you commands will be shown
+       _ ->
+          IO.puts "You can only run or show commands. This is not a drill."
     end
   end
 end
