d5dc4c518b25f278642da9a5b4563c3497eccb0c
milica-nerlovic <milica.nerlovic@gmail.com>
2016-12-15 15:22:37 +0100

    Display command exit status after each command

--- lib/runner.ex
+++ lib/runner.ex
@@ -4,8 +4,14 @@ defmodule Wizard.Runner do
       {duration, output} = :timer.tc(fn ->
         :os.cmd(command["command_string"] |> String.to_char_list)
       end)
+      status = :os.cmd('if [ $? -eq 0 ]; then echo 0; else echo 1; fi')
+
       IO.puts "command: #{command["command_string"]}"
       IO.puts "duration: #{duration/1_000_000} seconds"
+      case status do
+        '0\n' -> IO.puts "exit status: passed"
+        '1\n' -> IO.puts "exit status: failed"
+      end
       IO.puts output
     end
   end
