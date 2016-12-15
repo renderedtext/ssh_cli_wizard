506e66ab48a9b58f5758212cb3184f0556ec3b88
milica-nerlovic <milica.nerlovic@gmail.com>
2016-12-15 13:24:18 +0100

    Fix syntax error

--- lib/wizard.ex
+++ lib/wizard.ex
@@ -6,10 +6,10 @@ defmodule Wizard do
     job = Parser.parse
 
     [user_command | user_command_set] = args
-    [command_set | rest] = user_command_set
 
     case user_command do
       "run" ->
+          command_set = user_command_set |> List.first
           if job |> Map.get("commands") |> Map.has_key?(command_set) do
            Parser.commands(job, command_set) |> Runner.execute
           else
@@ -17,6 +17,7 @@ defmodule Wizard do
           end
       "show" ->
           #here all you commands will be shown
+          IO.puts "something"
        _ ->
           IO.puts "You can only run or show commands. This is not a drill."
     end
