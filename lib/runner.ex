704347998e0c555f9f026d3d82e660f3767f2ce1
milica-nerlovic <milica.nerlovic@gmail.com>
2016-12-15 14:55:37 +0100

    Meassue the time needed to run a command and display it to the user

--- lib/runner.ex
+++ lib/runner.ex
@@ -1,7 +1,12 @@
 defmodule Wizard.Runner do
   def execute(commands) do
     Enum.each commands, fn(command) ->
-      IO.puts :os.cmd(command["command_string"] |> String.to_char_list)
+      {duration, output} = :timer.tc(fn ->
+        :os.cmd(command["command_string"] |> String.to_char_list)
+      end)
+      IO.puts "command: #{command["command_string"]}"
+      IO.puts "duration: #{duration/1_000_000} seconds"
+      IO.puts output
     end
   end
 end
